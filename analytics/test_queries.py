c4cc3a774c57cdafcf1af97754b3c9b241a26729
ShreyaP-19 <shreyapera@gmail.com>
2026-06-17 13:03:57 +0530

    Add files via upload

---
+++ analytics/test_queries.py
@@ -0,0 +1,19 @@
+from sql_queries import *
+
+print("\nTop Views:")
+print(top_videos_by_views())
+
+print("\nTop Engagement:")
+print(top_videos_by_engagement())
+
+print("\nTop Like Rate:")
+print(top_like_rate())
+
+print("\nTop Comment Rate:")
+print(top_comment_rate())
+
+print("\nDuration Impact:")
+print(duration_vs_engagement())
+
+print("\nPosting Trends:")
+print(posting_trends())
